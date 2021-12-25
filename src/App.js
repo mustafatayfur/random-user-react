c00333917348f403fd3d076b72afe6ef95aa035a
mustafa <mltm0204@gmail.com>
2021-12-26 00:29:33 +0300

    second commit

--- src/App.js
+++ src/App.js
@@ -1,35 +1,110 @@
-/** @format */
-
-import { Button, Card, Col, Image, Row } from "react-bootstrap";
+import { Button, Card, Col, Container, Image, Row } from "react-bootstrap";
 import "./App.css";
+// import Icon from "./components/Icon";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import man from "./assets/man.svg";
+import woman from "./assets/woman.svg";
+import mail from "./assets/mail.svg";
+import growingMan from "./assets/growing-up-man.svg";
+import growingWoman from "./assets/growing-up-woman.svg";
+import map from "./assets/map.svg";
+import padlock from "./assets/padlock.svg";
+import phones from "./assets/phone.svg";
 
 function App() {
+  const [users, setUsers] = useState([]);
+  const [values, setValues]= useState("");
+  const [title, setTitle] = useState("");
+
+  const baseUrl = "https://randomuser.me/api/";
+  const getUsers = async () => {
+    // const res = await axios.get(baseUrl);
+    const { data } = await axios.get(baseUrl);
+    // console.log(data);
+    // console.log(data.results);
+    setUsers(data.results);
+    console.log(users);
+  };
+  useEffect(() => {
+    getUsers();
+  }, []);
+  const handleMouseOver = (e) => {
+    return ( 
+        e.target.value
+      )
+  };
   return (
-    <div className='App'>
-      <Card className='text-center'>
-        <Card.Header>Featured</Card.Header>
-        <Card.Body>
-          <Card.Title>Special title treatment</Card.Title>
-          <Card.Text>
-            With supporting text below as a natural lead-in to additional
-            content.
-          </Card.Text>
-          <Row>
-            <Col xs={6} md={4}>
-              <Image src='holder.js/171x180' roundedCircle />
-            </Col>
-          </Row>
-          <div className='btn'>
-            <Button variant='primary'>Go somewhere</Button>
-          </div>
-          <div className='btn'>
-            <Button variant='primary'>Go somewhere</Button>
-          </div>
-        </Card.Body>
-        <Card.Footer className='text-muted'>2 days ago</Card.Footer>
-      </Card>
+    <div className="App">
+      {users.map((user, index) => {
+        const { name, location, gender, email, phone, login, dob } = user;
+        return (
+          <Card className="text-center" key={index}>
+            <Card.Header className="header"></Card.Header>
+            <Row>
+              <Col>
+                <Image
+                  src={user.picture.large}
+                  className="user"
+                  roundedCircle
+                />
+              </Col>
+            </Row>
+            <Card.Body>
+              <Card.Text>
+                { `My ${e.target.value} is`}
+              </Card.Text>
+              <Card.Title>Special title treatment</Card.Title>
+              <Container>
+                <Row>
+                  <Col xs={6} md={4} lg={2}>
+                    <Image
+                      onMouseOver={()=> handleMouseOver()}
+                      src={gender === "male" ? man : woman}
+                      className="img"
+                      roundedCircle
+                      value={name.first}
+                    />
+                  </Col>
+                  <Col xs={6} md={4} lg={2}>
+                    <Image
+                      src={mail}
+                      className="img"
+                      roundedCircle
+                      value={email}
+                    />
+                  </Col>
+                  <Col xs={6} md={4} lg={2}>
+                    <Image
+                      src={gender === "male" ? growingMan : growingWoman}
+                      className="img"
+                      value={dob.age}
+                      roundedCircle
+                    />
+                  </Col>
+                  <Col xs={6} md={4} lg={2}>
+                    <Image src={map} className="img" value={location.street.number + location.street.name} roundedCircle />
+                  </Col>
+                  <Col xs={6} md={4} lg={2}>
+                    <Image src={phones} value={phone} className="img" roundedCircle />
+                  </Col>
+                  <Col xs={6} md={4} lg={2}>
+                    <Image src={padlock} value={login.password} className="img" roundedCircle />
+                  </Col>
+                  
+                </Row>
+              </Container>
+              <div className="btn">
+                <Button variant="primary">NEW USER</Button>
+              </div>
+              <div className="btn">
+                <Button variant="primary">ADD USER</Button>
+              </div>
+            </Card.Body>
+          </Card>
+        );
+      })}
     </div>
   );
 }
-
 export default App;
